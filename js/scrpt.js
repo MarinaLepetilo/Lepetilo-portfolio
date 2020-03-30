351bdafb264cbdbbc2e1b74a449a115d1d1e90a6
MLepetilo <marina.lepetilo@gmail.com>
2020-03-30 06:09:18 +0300

    video

--- js/scrpt.js
+++ js/scrpt.js
@@ -38,22 +38,22 @@ $(document).ready(function() {
 
 /* VIDEO */
 $(function(){
-    var $refreshButton = $('#refresh');
-    var $results = $('#css_result');
-    
-    function refresh(){
-      var css = $('style.cp-pen-styles').text();
-      $results.html(css);
-    }
+  var $refreshButton = $('#refresh');
+  var $results = $('#css_result');
   
-    refresh();
-    $refreshButton.click(refresh);
-    
-    // Select all the contents when clicked
-    $results.click(function(){
-      $(this).select();
-    });
+  function refresh(){
+    var css = $('style.cp-pen-styles').text();
+    $results.html(css);
+  }
+
+  refresh();
+  $refreshButton.click(refresh);
+  
+  // Select all the contents when clicked
+  $results.click(function(){
+    $(this).select();
   });
+});
 
   $('.slider').slick({
     slidesToShow: 1,
@@ -100,3 +100,7 @@ lightbox.addEventListener('click', e => {
   if (e.target !== e.currentTarget) return
   lightbox.classList.remove('active')
 })
+
+
+
+
